5968ebd263ba79d3ad0477424d9dabe31c420f15
nahla77 <115958949+nahla77@users.noreply.github.com>
2025-04-10 18:48:45 +0200

    Add files via upload

---
+++ src/test/java/HomePageTCs.java
@@ -0,0 +1,35 @@
+
+import DriverFactory.Driver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import Pages.HomePage;
+
+public class HomePageTCs {
+
+    public ThreadLocal<Driver> driver;
+
+    @BeforeMethod
+    @Parameters(value = {"browserName"})
+    public void setup(@Optional("CHROME") String browserName) {
+        driver = new ThreadLocal<>();
+        driver.set(new Driver(browserName));
+        driver.get().browser().navigateToURL("https://practicesoftwaretesting.com/");
+    }
+
+    @Test
+    public void testAllLinksOnHomePage() {
+
+        HomePage homePage = new HomePage(driver.get());
+
+        homePage.verifyAllLinksAreWorking();
+    }
+
+    @AfterMethod
+    public void tearDown() throws InterruptedException {
+        Thread.sleep(3000);
+        driver.get().Quit();
+    }
+}
